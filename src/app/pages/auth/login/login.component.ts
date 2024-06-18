e66ca3911557c7bf92069402134ea9061dae9efc
donaderoyan <donaderoyan@gmail.com>
2024-06-18 10:05:52 +0700

    simulate login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -6,13 +6,15 @@ import { Router, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
-
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
     selector: 'login.component',
     templateUrl: './login.component.html',
     standalone: true,
-    imports: [CommonModule, CardModule, ButtonModule, RouterLink, FormsModule, ReactiveFormsModule, ButtonModule, InputTextModule,]
+    imports: [CommonModule, ToastModule, CardModule, ButtonModule, RouterLink, FormsModule, ReactiveFormsModule, ButtonModule, InputTextModule,],
+    providers: [MessageService]
 })
 export class LoginComponent implements OnInit {
     public credential = { // Hardcode user
@@ -24,7 +26,7 @@ export class LoginComponent implements OnInit {
         password : FormControl<string>,
     }>;
     isloading!: boolean | false
-    constructor(private router: Router) {
+    constructor(private router: Router, private messageService: MessageService) {
         this.initFormGroup();
     }
     public ngOnInit() : void {}
@@ -43,11 +45,19 @@ export class LoginComponent implements OnInit {
     }
 
     public async onClickSubmit() : Promise<void> {
+        this.isloading = true
         if (this.formGroup.controls.email.value !== this.credential.email || this.formGroup.controls.password.value !== this.credential.password) {
-            console.log("Login failed");
+            this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Login failed please check again your email or password' });
+            setTimeout(() => {
+                this.isloading = false
+            }, 2000);
         } else {
-            localStorage.setItem('token', 'secret');
-            this.router.navigate(['/home']);
+            this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Login succeed' });
+            setTimeout(() => {
+                localStorage.setItem('token', 'secret');
+                this.isloading = false
+                this.router.navigate(['/home']);
+            }, 2000);
         }
     }
 }
